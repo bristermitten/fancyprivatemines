e5f6153e6f1af9e9a465513913a0502361b627f4
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-11-17 11:11:52 +0000

    Add kotlinx.serialization

--- fpm-base/src/main/kotlin/me/bristermitten/fancyprivatemines/data/RelativeLocation.kt
+++ fpm-base/src/main/kotlin/me/bristermitten/fancyprivatemines/data/RelativeLocation.kt
@@ -1,5 +1,9 @@
 package me.bristermitten.fancyprivatemines.data
 
+import kotlinx.serialization.Serializable
+import org.bukkit.Location
+
+@Serializable
 data class RelativeLocation(
         val dx: Double,
         val dy: Double,
@@ -7,4 +11,12 @@ data class RelativeLocation(
         val dYaw: Float = 0f,
         val dPitch: Float = 0f,
 ) {
+    fun toLocation(base: Location): Location {
+        return base.clone()
+                .add(dx, dy, dz,)
+                .apply {
+                    yaw += dYaw
+                    pitch += dPitch
+                }
+    }
 }
